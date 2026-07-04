3633d8bcf940b7fcfe8a251f60ae62d7c565b331
Mash <mariamghalechyan3@gmail.com>
2026-07-05 02:04:52 +0400

    Point frontend to live Railway backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: 'http://localhost:8000' })
+const api = axios.create({ baseURL: 'https://docuai-production.up.railway.app' })
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token')
